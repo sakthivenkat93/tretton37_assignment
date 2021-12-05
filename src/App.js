4df0ac3feb8a31bbd93193d8161d716d9de25724
Sakthi Priyadharshan Venkatesan <sakthivenkat93@gmail.com>
2021-12-05 22:32:57 +0530

    Resolve Unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import EmployeeCard from './EmployeeCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { setEmployees, setFilterName, setFilterOffice } from './EmployeeSlice';
 
