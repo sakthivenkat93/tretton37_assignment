9e8f0befa6a5cde4306ccae7637560b704a48018
Sakthi Priyadharshan Venkatesan <sakthivenkat93@gmail.com>
2021-12-05 20:05:50 +0530

    Dashboard with basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import EmployeeCard from './EmployeeCard';
 
-function App() {
+require('dotenv').config()
+
+const App = () => {
+  let [Employees, setEmployees] = useState([]);
+
+  fetch('https://api.1337co.de/v3/employees', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': process.env.REACT_APP_SERVER_API_KEY
+    },
+  }).then(response => response.json())
+    .then(data => {
+      console.log('Success:', setEmployees(data));
+    })
+
+  const listItems = Employees.map((Employee, index) =>
+    <EmployeeCard key={index} Employee={Employee}></EmployeeCard>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppHeader">
+        The fellowship of the tretton37
+      </div>
+      <div className="AppBody">
+        <div className="FilterArea">
+          <span>
+            Potential Filter and Tools Area
+          </span>
+        </div>
+        <div className="EmployeeCardCollection">
+          {listItems.length > 0 ? listItems : <div className="LoadingPlaceHolder">Loading</div>}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/EmployeeCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './EmployeeCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGitSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+const EmployeeCard = (props) => {
+    return(
+        <div className="EmployeeCard">
+            <div className="ImageContainer">
+                <div className="Image" style={{background:`url(${props.Employee.imagePortraitUrl}) no-repeat center`, backgroundSize: 'contain'}}>
+                </div>
+            </div>
+            <div className="EmployeeContact">
+                <div className="EmployeeName">
+                    {props.Employee.name}
+                </div>
+                <div className="EmployeeLinks">
+                <a href={`https://linkedin.com/${props.Employee.linkedIn}`}><FontAwesomeIcon icon={faLinkedin} color="#000000"/></a>
+                <a href={`https://github.com/${props.Employee.gitHub}`}><FontAwesomeIcon icon={faGitSquare} color="#000000"/></a>
+                <a href={`https://twitter.com/${props.Employee.twitter}`}><FontAwesomeIcon icon={faTwitterSquare} color="#000000"/></a>
+                </div>
+            </div>
+            <div className="EmployeeLocation">
+                Office: {props.Employee.office}
+            </div>
+        </div>
+    );
+}
+
+export default EmployeeCard;
